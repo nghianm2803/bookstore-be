8c5c91a06e84d38b814d3596b280178988e30767
nghia <nghianm2803@gmail.com>
2023-06-07 16:25:52 +0700

    update book api

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.status(200).send("Welcome to CoderSchool!");
+  res.status(200).send("Welcome to my Github!");
 });
+
+/* Book router */
+const bookRouter = require("./book.api.js");
+router.use("/books", bookRouter);
 
 module.exports = router;
